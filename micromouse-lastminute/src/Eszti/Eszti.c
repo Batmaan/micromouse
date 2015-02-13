3a9098a89372e490a2423262507a82eb39dc5321
Daniel <Daniel@192.168.0.11>
2015-02-13 19:59:38 +0100

    Na természetesen jó így is, headerben mindent el tudunk intézni úgyis. Az include mappába nem tudunk csak úgy csinálni header fáljt nemtudom miért, szóval lehet vissza kéne térni a header+source dologhoz, ezt majd eldöntjük, egyelőre úgysem ezzel fogjuk tölteni az időt

--- micromouse-lastminute/src/Eszti/Eszti.c
+++
@@ -1,8 +0,0 @@
-/*
- * Eszti.c
- *
- *  Created on: 13 Feb 2015
- *      Author: Daniel
- */
-#include "Eszti/Eszti.h"
-
--- micromouse-lastminute/src/Poli/Poli.c
+++
@@ -1,8 +0,0 @@
-/*
- * Poli.c
- *
- *  Created on: 13 Feb 2015
- *      Author: Daniel
- */
-
-#include "Poli/Poli.h"
