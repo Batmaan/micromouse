bc66c5a95ee8f1968371be580601c41a7fc8b826
Daniel <Daniel@BATMAN-PC>
2015-03-06 20:32:56 +0100

    Megcsináltam a PWM inicializálását, most írok egy kis függvényt amivel meg lehet változtatni a motorPWM kitöltési tényezőjét futásidő alatt is, ez a kezdeti lépés a PD szabályzóhoz. Megpróbálkozom még ma az encoderek incializálásával de azok elég geciknek tűnnek nekem sajnos

--- micromouse-lastminute/include/Dani/DaniPeripheral.h
+++ micromouse-lastminute/include/Dani/DaniPeripheral.h
@@ -79,72 +79,96 @@
 #define UDBDP_Pin GPIO_Pin_12
 #define UDBDP_Port GPIOA
 
-uint32_t ADC1ConvertedValue[2] = {0,0};
-uint32_t ADC2ConvertedValue[2] = {0,0};
-
+uint32_t ADC1ConvertedValue[2] =
+{ 0, 0 };
+uint32_t ADC2ConvertedValue[2] =
+{ 0, 0 };
 
 void USART1_Init(void)
 {
-		GPIO_InitTypeDef GPIO_InitStructure;
-        USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
 
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-        GPIO_InitStructure.GPIO_Pin =  USARTtx_Pin;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_Init(USARTtx_Port, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = USARTtx_Pin;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(USARTtx_Port, &GPIO_InitStructure);
 
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-        GPIO_InitStructure.GPIO_Pin = USARTrx_Pin ;
-        GPIO_Init(USARTrx_Port, &GPIO_InitStructure);
-        /*
-        - BaudRate = 115200 baud
-        - Word Length = 8 Bits
-        - One Stop Bit
-        - No parity
-        - Hardware flow control disabled (RTS and CTS signals)
-        */
-        USART_InitStructure.USART_BaudRate = 9600;
-        USART_InitStructure.USART_WordLength = USART_WordLength_9b;
-        USART_InitStructure.USART_StopBits = USART_StopBits_1;
-        USART_InitStructure.USART_Parity = USART_Parity_No;
-        USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-        USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Pin = USARTrx_Pin;
+	GPIO_Init(USARTrx_Port, &GPIO_InitStructure);
+	/*
+	 - BaudRate = 115200 baud
+	 - Word Length = 8 Bits
+	 - One Stop Bit
+	 - No parity
+	 - Hardware flow control disabled (RTS and CTS signals)
+	 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_9b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl =
+			USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-        /* Configure the USART1 */
-        USART_Init(USART1, &USART_InitStructure);
-        /* Enable USART1 interrupt */
-        USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-        //USART_ITConfig(USART1,USART_IT_TXE,ENABLE);
-        /* Enable the USART1 */
-        USART_Cmd(USART1, ENABLE);
+	/* Configure the USART1 */
+	USART_Init(USART1, &USART_InitStructure);
+	/* Enable USART1 interrupt */
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	//USART_ITConfig(USART1,USART_IT_TXE,ENABLE);
+	/* Enable the USART1 */
+	USART_Cmd(USART1, ENABLE);
 }
 
 void MotorInit(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef TIM_OCStruct;
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	GPIO_InitStructure.GPIO_Pin = balMotPWM_Pin;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(balMotPWM_Port,&GPIO_InitStructure);
+	GPIO_Init(balMotPWM_Port, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = jobbMotPWM_Pin;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(jobbMotPWM_Port,&GPIO_InitStructure);
+	GPIO_Init(jobbMotPWM_Port, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = balMotDIR_Pin;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(balMotDIR_Port,&GPIO_InitStructure);
+	GPIO_Init(balMotDIR_Port, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = jobbMotDIR_Pin;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(jobbMotDIR_Port,&GPIO_InitStructure);
+	GPIO_Init(jobbMotDIR_Port, &GPIO_InitStructure);
 
+	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseStructure.TIM_Prescaler = 0;
+	TIM_TimeBaseStructure.TIM_Period = 4095;
+
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM3, ENABLE);
+
+	TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+
+    TIM_OCStruct.TIM_Pulse = 0; /* CH2 jobbmotor */
+    TIM_OC2Init(TIM3, &TIM_OCStruct);
+    TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+    TIM_OCStruct.TIM_Pulse = 0; /* CH4 balmotor */
+    TIM_OC4Init(TIM3, &TIM_OCStruct);
+    TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
 }
 void ADCInit(void)
 {
@@ -152,20 +176,19 @@ void ADCInit(void)
 	DMA_InitTypeDef DMA_InitStruct;
 	ADC_InitTypeDef ADC_InitStructure;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1| RCC_APB2Periph_ADC2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2, ENABLE);
 
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 ;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4
+			| GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	//
-	//DMA
+	//DMA CH1
 	//
 	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t) & ADC1->DR; //ADC13's data register
-	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) & ADC1ConvertedValue;
-	//UTÁNANÉZNI
-	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralDST; //KURVÁRAJÓKÉRDÉSHOGYJÓE!!!!!
-	//UTÁNANÉZNI
+	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) &ADC1ConvertedValue;
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
 	DMA_InitStruct.DMA_BufferSize = 2;
 	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
@@ -173,22 +196,20 @@ void ADCInit(void)
 	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord; //Stores 16 bit values
 	DMA_InitStruct.DMA_Mode = DMA_Mode_Circular;
 	DMA_InitStruct.DMA_Priority = DMA_Priority_High;
-	//DMA_Init(DMA1_Channel1,	&DMA_InitStruct);
 	DMA_Init(DMA1_Channel1, &DMA_InitStruct);
 	//
-	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t) & ADC2->DR; //ADC13's data register
-	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) & ADC2ConvertedValue;
-	//UTÁNANÉZNI
-	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralDST; //KURVÁRAJÓKÉRDÉSHOGYJÓE!!!!!
-	//UTÁNANÉZNI
-	DMA_InitStruct.DMA_BufferSize = 2;
-	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	//DMA CH2
+	//
+	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t) & ADC2->DR; //ADC13's data register ugye ahonnan
+	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t) &ADC2ConvertedValue; // globális változó pointer
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC; //peripherial source,  nem destination
+	DMA_InitStruct.DMA_BufferSize = 2; //2 változót ir ugye a tömbbe
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable; //ez nemtudmo mi
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable; //ez sem
 	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord; //Reads 16 bit values
 	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord; //Stores 16 bit values
-	DMA_InitStruct.DMA_Mode = DMA_Mode_Circular;
-	DMA_InitStruct.DMA_Priority = DMA_Priority_High;
-	//DMA_Init(DMA1_Channel2,	&DMA_InitStruct);
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Circular; // mégjobb kérdés
+	DMA_InitStruct.DMA_Priority = DMA_Priority_High; //ez viszont elég érthetõ
 	DMA_Init(DMA1_Channel2, &DMA_InitStruct);
 	//
 	//ADC
@@ -196,46 +217,45 @@ void ADCInit(void)
 	ADC_InitStructure.ADC_NbrOfChannel = 2;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-	ADC_InitStructure.ADC_ExternalTrigConv= 0;
+	ADC_InitStructure.ADC_ExternalTrigConv = 0;
 	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
 	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
-	ADC_Init(ADC1, &ADC_InitStructure );
-
+	ADC_Init(ADC1, &ADC_InitStructure);
+//
 	ADC_InitStructure.ADC_NbrOfChannel = 2;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-	ADC_InitStructure.ADC_ExternalTrigConv= 0;
+	ADC_InitStructure.ADC_ExternalTrigConv = 0;
 	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
 	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
-	ADC_Init(ADC2,&ADC_InitStructure );
-
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_2,1,ADC_SampleTime_71Cycles5);
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_3,2,ADC_SampleTime_71Cycles5);
-
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_4,1,ADC_SampleTime_71Cycles5);
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_5,2,ADC_SampleTime_71Cycles5);
-
+	ADC_Init(ADC2, &ADC_InitStructure);
+//
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_2, 1, ADC_SampleTime_71Cycles5);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_3, 2, ADC_SampleTime_71Cycles5);
+//
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_4, 1, ADC_SampleTime_71Cycles5);
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_5, 2, ADC_SampleTime_71Cycles5);
+//
 	ADC_DMACmd(ADC1, ENABLE);
 	ADC_DMACmd(ADC2, ENABLE);
-
+//
 	ADC_Cmd(ADC1, ENABLE);
 	ADC_Cmd(ADC2, ENABLE);
-
+//
 	ADC_SoftwareStartConvCmd(ADC1, ENABLE);
 	ADC_SoftwareStartConvCmd(ADC2, ENABLE);
 }
 
-void PinInit(void) {
+void PinInit(void)
+{
 	GPIO_InitTypeDef GPIO_InitStructure;
 
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
-    USART1_Init();
-    MotorInit();
-
-
+	USART1_Init();
+	MotorInit();
 
 }
 
@@ -246,7 +266,5 @@ void Dani_Init(void)
 	MotorInit();
 	USART1_Init();
 }
-
-
 
 #endif /* DANI_DANI_H_ */
